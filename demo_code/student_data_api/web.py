290d255fcaf2fdbe61e821e71c6aace8f180b217
Kristofferson Culmer <krcd58@mail.missouri.edu>
2024-06-25 11:21:07 -0500

    Add files via upload

---
+++ demo_code/student_data_api/web.py
@@ -0,0 +1,30 @@
+import flask
+from flask import request, jsonify
+import student_generator_v2 as sg
+
+#create an app
+app = flask.Flask(__name__)
+
+#Use student generator program as a base
+#Write a function to create and return a list of student dictionaries
+#Create 2 routes
+# - return all students
+# - returns student by id 
+
+#Auto reload server when changes made
+app.config["DEBUG"] = True
+
+#load student dictionaries
+student_dictionaries = sg.get_student_dictionaries()
+
+@app.route('/', methods=['GET'])
+def index():
+    return "<h1> My name is Kristofferson Culmer</h1>"
+
+#Create route to return all student data
+@app.route('/api/students/all', methods=['GET'])
+def api_all():
+    return jsonify(student_dictionaries) 
+
+app.run()
+
